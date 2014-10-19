ad47146a2995754d07f7f0fb59709aa4ef8d51e3
Jorge Valdez <valdez.a.jorge@gmail.com>
2014-10-19 04:26:49 -0500

    Initial commit
    Socket.io implementation with Node.JS dependency. Draws to the
    Display.html on localhost port 3000

---
+++ DoodleTV/DoodleTV/Html/javascript/NodeServer.js
@@ -0,0 +1,44 @@
+﻿//////////
+
+var express = require('express')
+var app = express();
+app.use(express.static(__dirname));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+	res.sendfile('Display.html');
+});
+
+io.on('connection', function (socket) {
+	console.log('a user connected');
+
+	socket.on('message',function(event){ 
+		console.log('Received message from client!', event)
+	});
+
+	socket.on('image', function (dataUrl) {
+		/* CRAP////////////
+		var fs = require("node-fs");
+
+		var fileName = __dirname + "1SWAG.png";
+
+		fs.open(fileName, 'a', 0755, function (err, fd) {
+			console.log(fileName);
+			if (err) throw err;
+
+			fs.write(fd, buffer, null, 'Binary', function (err, written, buff) {
+				fs.close(fd, function () {
+					console.log("File saved :)");
+				});
+			})
+		});
+		*////////////////
+
+		io.emit('image', dataUrl);
+	});
+});
+
+http.listen(3000, function () {
+	console.log('listening on *:3000');
+});
---
+++ DoodleTV/DoodleTV/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DoodleTV.Resources;
+
+namespace DoodleTV
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
